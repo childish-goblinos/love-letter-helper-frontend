f85e96822f0497ad385b01cfb04a31891b3c60f8
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-09-06 12:06:30 -0700

    updated .env.sample; added auth0 code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap/Container';
+
+// `withAuth0` is for `Class` components
+import { withAuth0 } from '@auth0/auth0-react';
+
+// import buttons and such
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 
 import './App.css';
@@ -9,14 +18,28 @@ class App extends React.Component
   {
     return (
       <>
-        <div className="App">
-          <p>Love letter FrontEnd</p>
-        </div>
+        <Container className="App">
+          <h1>Love letter FrontEnd</h1>
+
+          { // if authenticated, see the `LogoutButton`
+            // if not authenticated, see the `LoginButton`
+            this.props.auth0.isAuthenticated
+              ? <LogoutButton />
+              : <LoginButton />
+          }
+          { // if authenticated, see the profile page
+            // if not authenticated, see a message asking them to log in
+            this.props.auth0.isAuthenticated
+            ? <Profile/>
+            : <h2>Please log in c:</h2>
+          }
+        </Container>
       </>
     );
   }
-  
+
 }
 
-
-export default App;
+// note that this is different than what we're used to  
+// this line allows us to use `auth0` as props 
+export default withAuth0(App);
--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import App from './App';
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.REACt_APP_AUTH_DOMAIN}
-    clientId={process.env.REACt_APP_AUTH_CLIENT_ID}
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
 
     // redirect user to this page after login:
-    redirectUri={process.env.REACt_APP_AUTH_REDIRECT_URI}
+    redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
   >
     <App />
   </Auth0Provider>,
